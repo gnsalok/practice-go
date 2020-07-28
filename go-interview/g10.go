87ef15273e6de1c35091b5ffea59038788aabfbd
Alok Tripathi <gnsalok@gmail.com>
2020-07-28 14:57:15 +0530

    switch case in go

--- go-interview/g10.go
+++ go-interview/g10.go
@@ -1,40 +1,23 @@
-package main
+package main 
 
-import (
-	"fmt"
-)
+import "fmt"
+
+func location(city string) (string, string) {
+	var region string
+	var continent string
+
+	switch city {
+	case "Los Angeles", "LA", "Santa Monica":
+		region, continent = "California", "North America"
+	case "New York", "NYC":
+		region, continent = "New York", "North America"
+	default:
+		region, continent = "Unknown", "Unknown"
+	}
+	return region, continent
+}
 
 func main() {
-	
-	var wc int
-	fmt.Println("Enter the week count?")
-	fmt.Scanf("%v",&wc)
-
-	switch wc{
-	case 1:
-		fmt.Println("Sunday")
-		break
-	case 2:
-		fmt.Println("Monday")
-		break
-	case 3:
-		fmt.Println("Tuesday")
-		break
-	case 4:
-		fmt.Println("Wednesday")
-		break
-	case 5:
-		fmt.Println("Thursday")
-		break
-	case 6:
-		fmt.Println("Friday")
-		break
-	case 7:
-		fmt.Println("Saturday")
-		break
-	default:
-		fmt.Println("Select valid option")
-		break
-	}
-
+	region, continent := location("Santa Monica")
+	fmt.Printf("Matt lives in %s, %s", region, continent)
 }
