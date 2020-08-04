ab5e05f87a5e71eea657022a9db1b6411af66728
Alok Tripathi <gnsalok@gmail.com>
2020-08-04 09:22:14 +0530

    updating interview answer

--- golang-all/Interface/polymorphism.go
+++ golang-all/Interface/polymorphism.go
@@ -1,48 +1,45 @@
-package main 
+package main
 
-
-import(
+import (
 	"fmt"
 	"math"
 )
 
-func main(){
-rect := Rectangle{20,50}
-cir := Circle{2}
+func main() {
+	rect := Rectangle{20, 50}
+	cir := Circle{2}
 
+	//This is the way to get the resutlt via receiver function or methods.
+	fmt.Println("Area of Rectangle = ", rect.area())
+	fmt.Println("Area of Cirle is = ", cir.area())
 
-//This is the way to get the resutlt via receiver function or methods.
-fmt.Println("Area of Rectangle = ", rect.area())
-fmt.Println("Area of Cirle is = ",cir.area())
-
- 
-//This is Polymorphism   (we got the same result) 
-fmt.Println("Area of Rectangle =",getArea(rect))
-fmt.Println("Area of Circle =",getArea(cir))
+	//This is Polymorphism   (we got the same result)
+	fmt.Println("Area of Rectangle =", getArea(rect))
+	fmt.Println("Area of Circle =", getArea(cir))
 
 }
 
-type Shape interface{
-	 area() float64
+type Shape interface {
+	area() float64
 }
 
-type Rectangle struct{
+type Rectangle struct {
 	height float64
-	width float64
+	width  float64
 }
 
-type Circle struct{
+type Circle struct {
 	radius float64
 }
 
-func(r Rectangle) area() float64{
+func (r Rectangle) area() float64 {
 	return r.height * r.width
 }
 
-func(c Circle) area() float64{
-	return math.Pi * math.Pow(c.radius,2)
+func (c Circle) area() float64 {
+	return math.Pi * math.Pow(c.radius, 2)
 }
 
-func getArea(shape Shape) float64{
+func getArea(shape Shape) float64 {
 	return shape.area()
 }
