890a8bb430bc92b84cc46fc9ebf8f5e8f5a1c144
Alok Tripathi <gnsalok@gmail.com>
2020-09-10 17:29:51 +0530

    added channels and goroutines

--- golang-all/Concurrency/app.go
+++ golang-all/Concurrency/app.go
@@ -1,10 +1,53 @@
-package main 
+package main
 
-import(
+import (
 	"fmt"
+	"net/http"
+	"time"
 )
 
-func main(){
+func main() {
+	links := []string{
+		"https://google.com",
+		"https://amazon.com",
+		"https://facebook.com",
+		"https://stackoverflow.com",
+	}
 
-	
+	c := make(chan string)
+
+	for _, link := range links {
+		go checkLink(link, c)
+	}
+
+	// for i := 0; i < len(links); i++ {
+	// 	fmt.Println(<-c)
+	// }
+
+	//option 1
+	// for {
+	// 	go checkLink(<-c, c)
+	// }
+
+	//better syntax
+	for l := range c {
+		time.Sleep(time.Second)
+		go checkLink(l, c)
+	}
+
+	fmt.Println(<-c)
+
+}
+
+func checkLink(link string, c chan string) {
+	_, err := http.Get(link)
+
+	if err != nil {
+		fmt.Println(link, "might down")
+		c <- link
+		return
+	} else {
+		fmt.Println(link, "is up")
+		c <- link
+	}
 }
